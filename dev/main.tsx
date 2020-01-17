f38059f87c28ec16f056243563c13d37297c08cf
himself65 <himself65@outlook.com>
2020-01-18 01:28:36 +0800

    feat: add dev environment

---
+++ dev/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const app = document.getElementById('app')
+
+// todo: bind es module
+ReactDOM.render((
+  <div>Hello, world</div>
+), app)
